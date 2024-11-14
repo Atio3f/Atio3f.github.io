80552595f29ed9f1a1055250af5447479152a1b7
Atio3f <mathiasfer38@gmail.com>
2024-11-15 00:06:34 +0100

    ajoutFoC
    Ajout du projet Fight of Cities sur la page projet

--- styles/script.js
+++ styles/script.js
@@ -1,4 +1,25 @@
 const val = {
+    "FoC" : `
+    DESCRIPTIF PROJET :
+	    Adaption de l'un de nos jeux de société en un jeu vidéo 2D sur Godot pour permettre de continuer à y jouer malgré la distance.
+    DESCRIPTION Fight Of Cities : 
+      Fight of Cities est un jeu de stratégie sur plateau. L'objectif est de détruire le cristal de l'adversaire pour remporter la partie.
+      Il faut ainsi constituer une petite armée, construire différents bâtiments et utiliser des objets afin de vous renforcer.
+      Les différentes races de créatures disponibles(pingouins, singes, taureaux, humains et chauves-souris) possèdent toutes des spécificités
+      particulières pour les exploiter au mieux et permet ainsi de faire la tactique à employer pour venir à bout de l'adversaire.
+      Les unités, bâtiments et objets sont tous des cartes à piocher et à poser.
+    NOTRE TRAVAIL :
+      Notre équipe de 3 personnes actuellement sur l'approfondissement des règles, d'un équilibrage général et de plusieurs tests de la version papier
+      tandis que je m'occupe, en tant qu'unique développeur, d'implémenter le maximum d'éléments permettant de retranscrire au mieux le système de jeu
+      et les mécaniques des différentes cartes.
+    TRAVAIL INFORMATIQUE DEJA EFFECTUE :
+      J'ai pour le moment réalisé la structure des unités de manière satisfaisante. J'ai également ajouter différentes classes pour implémenter
+      le système de capacités qui sera approfondi à chaque nouvelle unité rajoutée. Le système de déplacement et de terrain fonctionne très bien
+      également et la structure du jeu a été restructurée récemment pour permettre bientôt le multijoueur.
+    TRAVAIL INFORMATIQUE A VENIR :
+      Dans les prochains mois, j'implémenterai les bâtiments et les sorts puis je rajouterai le multijoueur
+      et enfin je designerai tout l'aspect menu et paramètres.`
+    ,
     "MEGASAE" : `
     DESCRIPTIF SAE :
    	 Mise en place d'un projet pour la réalisation d'une application permettant la gestion d'évènements pour les cirques.
@@ -9,7 +30,8 @@ const val = {
       réalisation de personas pour implémenter, création d'un diagramme de cas d'utilisation pour évaluer les attendus
       à réaliser lors de la phase de développement.
      INDIVIDUELLEMENT : 
-      Réalisation d'un processus de gestion du projet et participation à la création de l'interface de l'application sur JavaFX(pas encore fini du coup).
+      Réalisation d'un processus de gestion du projet et participation à la création de l'interface de l'application sur JavaFX
+      et de la partie implémentation des classes.
 
     COMPÉTENCES ACQUISES :
       Déroulé d'un projet avec une phase d'avant-projet pour préciser les attendus, recherche du profil de la clientèle attendue,
@@ -85,7 +107,11 @@ function updateText() {
     const selectElement = document.getElementById('Projets-list');
     const outputElement = document.getElementById('outputProjet');
     const selectedValue = selectElement.value;
-    
+    const FoCAnnexes = document.getElementsByClassName("FoC");
+    for (const annexe of FoCAnnexes){
+      annexe.style.display = "none"
+    }
+   
     if(selectedValue){
         message2 = `${selectedValue} : \n <pre>${val[selectedValue]}</pre>`;
         let htmlMessage = message2.replace(/\n/, '<br>');
@@ -101,9 +127,15 @@ function updateText() {
         }else{
           document.getElementById("SAE1_05_06").style.display = "none"
         }
+        if(selectedValue == "FoC"){
+          for (const annexe of FoCAnnexes){
+            annexe.style.display = "flex";
+          }
+        }
     }else{
         outputElement.textContent = `Sélectionnez le projet que vous voulez regarder au dessus.`
     }
+    
     
     
     /*outputElement.textContent = "Le texte sera mis à jour ici.";*/
@@ -124,3 +156,13 @@ function toggleMenu() {
   document.querySelector('.navbar').classList.toggle('active');
 }
 
+//Servira lorsque le php arrêtera ses caprices
+function baseValue(){
+  //document.getElementById('Projets-list').value = "FoC" ;
+  console.log(document.getElementById('Projets-list').value);
+ 
+}
+
+window.onload = function() {
+  baseValue();
+};
